40c40eaa937170443653111ff91f284cea2101f1
Kamil Purgat <kpurgat.zsp9@gmail.com>
2025-01-25 17:26:34 +0100

    Update header file

--- Core/Inc/temperature_sensor.h
+++ Core/Inc/temperature_sensor.h
@@ -7,3 +7,5 @@
 bool temperature_sensor_init(void);
 float temperature_sensor_get_temperature(void);
 HAL_StatusTypeDef temperature_sensor_set_alarm(float high_temperature, float low_temperature);
+bool temperature_sensor_is_alarm_triggered(void);
+void temperature_sensor_clear_alarm(void);
