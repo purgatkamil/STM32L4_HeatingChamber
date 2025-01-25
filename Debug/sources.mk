b3712d16b13c63ab84843b1417ed7112b1d16d48
Kamil Purgat <kpurgat.zsp9@gmail.com>
2025-01-25 17:38:20 +0100

    Change the project structure

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -22,6 +22,8 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
+App/lcd \
+App/temperature_sensor \
 Core/Src \
 Core/Startup \
 Drivers/STM32L4xx_HAL_Driver/Src \
